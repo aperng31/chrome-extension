6581da96d332df9212b7471aa3b6738d1f75d820
Danny Zheng <dannyzheng159@gmail.com>
2022-11-16 22:53:08 -0500

    Added constructor

--- main.js
+++ main.js
@@ -1,15 +1,57 @@
-// const body = document.body;
+class Square {
+    constructor(el) {
+        this.node = document.createElement('div');
+        this.node.setAttribute('id', 'square');
+        el.appendChild(this.node);
 
-const square = document.createElement("div");
-square.id = "square";
+        this.node.style.bottom = '100px';
+        this.node.style.left = '100px';
+
+        this.node.onmousedown = this.move;
+    }
+
+    move(event) {
+        const mySquare = this.node;
+        // (1) prepare to moving: make absolute and on top by z-index
+        mySquare.style.position = 'absolute';
+        mySquare.style.zIndex = 1000;
+        
+        // move it out of any current parents directly into body
+        // to make it positioned relative to the body
+        // document.body.append(ball);
+        
+        // centers the ball at (pageX, pageY) coordinates
+        function moveAt(pageX, pageY) {
+            mySquare.style.left = pageX - mySquare.offsetWidth / 2 + 'px';
+            mySquare.style.top = pageY - mySquare.offsetHeight / 2 + 'px';
+        }
+        
+        // move our absolutely positioned ball under the pointer
+        moveAt(event.pageX, event.pageY);
+        
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+        }
+        
+        // (2) move the ball on mousemove
+        document.addEventListener('mousemove', onMouseMove);
+        
+        // (3) drop the ball, remove unneeded handlers
+        mySquare.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            mySquare.onmouseup = null;
+            mySquare.style.position = 'fixed';
+        };
+    };
+}
+
+const square = new Square(document.body);
 const img = new Image();
 square.appendChild(img);
 const input = document.querySelector("input");
 // input.onchange = fn();
 // input.onchange(() => console.log(input.value));
 input.addEventListener('input', fn);
-
-document.body.appendChild(square);
 
 function fn() {
     //api.giphy.com/v1/gifs/search
@@ -28,16 +70,3 @@ function fn() {
     // return input.value;
 }
 
-// fetch('https://curriculum-api.codesmith.io/messages')
-// .then((data) => data.json())
-// .then((data) => {
-//   console.log(data);
-//   //iterate through data to get each message
-//   for (let i = 0; i < data.length; i++) {
-//     //dom manipulation to add each comment in the window for users to see.
-//     let messageList = document.getElementById('message-history');
-//     let message = document.createElement('li');
-//     message.innerHTML = `user: ${data[i].created_by} <br> message: ${data[i].message} <br> date posted: ${data[i].created_at}`;
-//     messageList.appendChild(message);
-//   }
-// });
